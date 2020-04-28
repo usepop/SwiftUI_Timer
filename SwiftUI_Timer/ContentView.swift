d6350a7af4ac055c3613d4102f903ce5f4ba3e48
dy.lin <usepop912@gmail.com>
2020-04-28 16:18:28 +0800

    Initial Commit

---
+++ SwiftUI_Timer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Timer
+//
+//  Created by 林定佑 on 2020/4/28.
+//  Copyright © 2020 Gearraido. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
