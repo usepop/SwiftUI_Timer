086ac7e681bad6bab1fb4690e830e80a14c8e46e
dy.lin <usepop912@gmail.com>
2020-04-28 20:23:28 +0800

    Practice swift timer

--- SwiftUI_Timer/ContentView.swift
+++ SwiftUI_Timer/ContentView.swift
@@ -9,8 +9,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var timer: Timer?
+    @State var timeflag = false
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Hello, World!")
+            Button(action: {
+                if self.timeflag == false {
+                    self.timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { timer in
+                        print("run timer")
+                    })
+                } else {
+                    self.timer?.invalidate()
+                    print("stop")
+                }
+                self.timeflag.toggle()
+            }) {
+                Text(self.timeflag ? "stop timer": "start timer")
+            }
+        }
     }
 }
 
